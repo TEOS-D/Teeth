436689d2b741289e4c745b65973730a6e0e70cb2
TEOS_D <laysandblack@gmail.com>
2022-09-13 16:44:36 +0300

    new file:   css/slick.css 	modified:   css/style.css 	modified:   index.html 	modified:   js/main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,22 @@
-$(function(){
-  $('.menu__btn').on('click', function(){
+$(function () {
+  $('.menu__btn').on('click', function () {
     $('.menu__list').toggleClass('menu__list--active');
   });
+
+  // $('.card__inner').slick({
+  //   slidesToShow: 1,
+  //   slidesToScroll: 1,
+  //   // asNavFor: '.slider-for',
+  //   // dots: true,
+  //   centerMode: true,
+  //   focusOnSelect: true,
+  //   variableWidth: true,
+  //   responsive: [{
+  //     breakpoint: 1200,
+  //     settings: 'slick'
+  //   }]
+  // });
+  $('.card__inner').slick({responsive: [{
+        breakpoint: 1200,
+       }]});
 });
